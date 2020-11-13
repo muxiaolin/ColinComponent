4d03b1f45b867b2bb55220a807d8d636a571be76
penglin <penglin@himango.cn>
2020-11-13 12:36:52 +0800

    配置发布

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ColinComponent
 android常用公用组件
+
+
+librestraming : 修改了librestreaming项目， https://github.com/lakeinchina/librestreaming
--- app/src/main/java/com/muxiaolin/app/MainApp.java
+++ app/src/main/java/com/muxiaolin/app/MainApp.java
@@ -3,8 +3,6 @@ package com.muxiaolin.app;
 
 import androidx.multidex.MultiDexApplication;
 
-import com.mgtj.airadio.base.BuildConfig;
-import com.mgtj.airadio.base.LibInitApp;
 
 /**
  * author : 彭林
@@ -16,6 +14,6 @@ public class MainApp extends MultiDexApplication {
     @Override
     public void onCreate() {
         super.onCreate();
-        LibInitApp.init(this, BuildConfig.DEBUG);
+//        LibInitApp.init(this, BuildConfig.DEBUG);
     }
 }
